fea16b8fd1455317d21ee483e7911d022b53607c
manmath <manmathtewaridps@gmail.com>
2025-06-02 14:57:16 +0530

    final python code

--- tokenization.py
+++ tokenization.py
@@ -17,27 +17,27 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
 tokenizer = Tokenizer(BPE())
 tokenizer.pre_tokenizer = Whitespace()
 trainer = BpeTrainer(vocab_size=100, special_tokens=["<unk>"])
+
 corpus = [
     "internationalization and internalization",
     "internationalization",
     "internalization",
     "inter", "national", "internal", "and"
 ]
+
 tokenizer.train_from_iterator(corpus, trainer)
 class SentenceRequest(BaseModel):
     text: str
-@app.get("/", response_class=HTMLResponse)
-async def serve_home():
-    with open("static/tokens.html", "r") as f:
-        html_content = f.read()
-        return HTMLResponse(content=html_content)
+
 @app.post("/tokenize")
-async def tokenize_text(request: SentenceRequest):
+def tokenize_text(request: SentenceRequest):
     encoded = tokenizer.encode(request.text)
+    print("Tokens:", encoded.tokens)
+    print("Token IDs: ", encoded.ids)
     return {
         "tokens": encoded.tokens,
         "ids": encoded.ids
